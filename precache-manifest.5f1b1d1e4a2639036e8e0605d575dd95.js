5de6d29d7827ed66a66dcad941ea50874feaf62e
mourya <mouryachakravarthi123@gmail.com>
2020-11-17 16:59:21 +0530

    Updates

---
+++ precache-manifest.5f1b1d1e4a2639036e8e0605d575dd95.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2daa5ce6882ae9feafafa8a7218fbf5",
+    "url": "/react_proj/index.html"
+  },
+  {
+    "revision": "a9676ab1442bd5c084e5",
+    "url": "/react_proj/static/css/main.10fa3c7c.chunk.css"
+  },
+  {
+    "revision": "9bdc2ab570d13a306fb4",
+    "url": "/react_proj/static/js/2.333c0205.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react_proj/static/js/2.333c0205.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9676ab1442bd5c084e5",
+    "url": "/react_proj/static/js/main.6305f1fc.chunk.js"
+  },
+  {
+    "revision": "50a7da1f98c25008c81f",
+    "url": "/react_proj/static/js/runtime-main.db97620c.js"
+  },
+  {
+    "revision": "48ae6434de48325549277b49f7ebf731",
+    "url": "/react_proj/static/media/arrow.48ae6434.svg"
+  },
+  {
+    "revision": "681afa2ed6c043ea96751e7b755cc561",
+    "url": "/react_proj/static/media/bolt.681afa2e.svg"
+  },
+  {
+    "revision": "e4f2d6b028d93b8c7988d2132bf13ca2",
+    "url": "/react_proj/static/media/github.e4f2d6b0.svg"
+  },
+  {
+    "revision": "23a7dee5995afbb2f9ad3b32799c6f82",
+    "url": "/react_proj/static/media/gmail.23a7dee5.svg"
+  },
+  {
+    "revision": "133ce3f86906cf46c6618abb24212cfe",
+    "url": "/react_proj/static/media/home.133ce3f8.svg"
+  },
+  {
+    "revision": "46ce74c2e7da57b99857891a9e0ebf94",
+    "url": "/react_proj/static/media/instagram.46ce74c2.svg"
+  },
+  {
+    "revision": "d849bc2557228a905cb46a21f1819d90",
+    "url": "/react_proj/static/media/linkedin.d849bc25.svg"
+  },
+  {
+    "revision": "8e62f3df7da42adc47a9a03749b69b63",
+    "url": "/react_proj/static/media/mouri.8e62f3df.png"
+  }
+]);
